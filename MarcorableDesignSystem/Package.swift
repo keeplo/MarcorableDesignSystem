9468505657efa3ac6597ca4ebf7059db02ba9a32
Keeplo <keepingitflow@gmail.com>
2023-07-30 22:53:37 +0900

    chore: .gitignore 추가 및 패키지 버전 제한 추가
    - iOS 15, swift v5, 5.8

--- MarcorableDesignSystem/Package.swift
+++ MarcorableDesignSystem/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MarcorableDesignSystem",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -24,5 +25,6 @@ let package = Package(
         .testTarget(
             name: "MarcorableDesignSystemTests",
             dependencies: ["MarcorableDesignSystem"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
